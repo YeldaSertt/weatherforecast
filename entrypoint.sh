d0d062b01b9828887375144acd85a40283a7053b
YeldaaSert <yeldaasert@gmail.com>
2022-05-30 21:24:53 +0300

    weatherforecast_setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,4 @@
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
-# python manage.py runscript add_dumy_data
 python manage.py runserver 0.0.0.0:8000
